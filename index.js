68ae791c7c6883aca367a054ffdcaae59cc487a8
Matthew Castillo <Blacksheep3y@gmail.com>
2020-06-14 19:55:04 -0400

    Created some files I think I may need

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require('express')
+const actionsRouter = require('./actions-router')
+const projectsRouter = require('./projects-router')
+const server = express()
+const port = 7000
+
+server.use(express.json())
+
+server.use('/actions', actionsRouter)
+server.use('/', projectsRouter)
+
+server.listen(port, () => {
+    console.log(`\n *** Server is listening on port ${port} *** \n`)
+})
+
+server.get('/', (req, res) => {
+    res.send("<h1>hello</h1>")
+})
