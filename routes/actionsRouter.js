a880b8108c73c1e27a6ea471081844dbf2a52005
Matt C <Blacksheep3y@gmail.com>
2020-08-16 19:49:47 -0400

    Finished Server file and started on actions and projects routers

---
+++ routes/actionsRouter.js
@@ -0,0 +1,7 @@
+  // import express
+  const express = require('express')
+  // use express on Router
+  const router = express.Router()
+  // require db to use functions
+  const ah = require('../data/helpers/actionModel')
+
---
+++ routes/projectsRouter.js
@@ -0,0 +1,6 @@
+  // import express
+  const express = require('express')
+  // use express on Router
+  const router = express.Router()
+  // require db to use functions
+  const ah = require('../data/helpers/projectModel')
